7ade688b2f3e4bce2e427fd9c26056cc1ab8e4ec
Mattia Dipaola <106329785+MetHope96@users.noreply.github.com>
2026-01-09 16:00:38 +0100

    Add ch01_ex07.cpp with a incorrectly nested comment example

---
+++ ch01_ex07.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    /* This is a comment
+       /* nested comment */ 
+       still inside comment
+    */
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
